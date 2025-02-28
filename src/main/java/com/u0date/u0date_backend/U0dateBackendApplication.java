ad7f979d936bccc1e86a31592b7109fa6a6dc600
3akare <bakaredavid007@gmail.com>
2025-02-28 15:23:09 +0100

    feat: configure database config

--- src/main/java/com/u0date/u0date_backend/U0dateBackendApplication.java
+++ src/main/java/com/u0date/u0date_backend/U0dateBackendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class U0dateBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(U0dateBackendApplication.class, args);
 	}
-
 }
