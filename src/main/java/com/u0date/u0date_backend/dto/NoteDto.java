e0bcde193745948fc618161a366793362d7177cb
3akare <bakaredavid007@gmail.com>
2025-03-04 18:20:23 +0100

    refactor: code refactor

--- src/main/java/com/u0date/u0date_backend/dto/NoteDto.java
+++ src/main/java/com/u0date/u0date_backend/dto/NoteDto.java
@@ -2,6 +2,7 @@ package com.u0date.u0date_backend.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
@@ -13,7 +14,9 @@ import java.time.LocalDateTime;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class NoteDto {
     private String id;
+    @NotNull(message = "title cannot be null or empty")
     private String title;
+    @NotNull(message = "content cannot be null or empty")
     private String content;
     private LocalDateTime updatedAt;
     private String lastEditedBy;
